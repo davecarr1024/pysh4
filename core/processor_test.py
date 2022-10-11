b6303489fbb41f4a14ad210964b9835b6ac01a47
Dave Carr <davecarr1024@gmail.com>
2022-10-11 10:17:22 -0400

    rule test

--- core/processor_test.py
+++ core/processor_test.py
@@ -19,6 +19,25 @@ def eq(value: int) -> _Rule:
     return rule
 
 
+result_combiner: processor.ResultCombiner[_Result] = sum
+
+
+def and_(*rules: _Rule) -> _Rule:
+    return processor.and_(rules, result_combiner)
+
+
+def zero_or_more(rule: _Rule) -> _Rule:
+    return processor.zero_or_more(rule, result_combiner)
+
+
+def one_or_more(rule: _Rule) -> _Rule:
+    return processor.one_or_more(rule, result_combiner)
+
+
+def zero_or_one(rule: _Rule) -> _Rule:
+    return processor.zero_or_one(rule, result_combiner)
+
+
 class EqTest(unittest.TestCase):
     def test_apply(self):
         for state, output in list[Tuple[_State, _StateAndResult]]([
@@ -67,3 +86,107 @@ class RefTest(unittest.TestCase):
             with self.subTest(state=state):
                 with self.assertRaises(errors.Error):
                     processor.ref('a')(_Scope({'a': eq(1)}), state)
+
+
+class OrTest(unittest.TestCase):
+    def test_apply(self):
+        for state, output in list[Tuple[_State, _StateAndResult]]([
+            ([1], ([], 1)),
+            ([2], ([], 2)),
+            ([1, 3], ([3], 1)),
+            ([2, 3], ([3], 2)),
+        ]):
+            with self.subTest(state=state, output=output):
+                self.assertEqual(
+                    processor.or_(eq(1), eq(2))(_Scope(), state),
+                    output
+                )
+
+    def test_apply_fail(self):
+        for state in list[_State]([
+            [],
+            [3],
+        ]):
+            with self.subTest(state=state):
+                with self.assertRaises(errors.Error):
+                    processor.or_(eq(1), eq(2))(_Scope(), state)
+
+
+class AndTest(unittest.TestCase):
+    def test_apply(self):
+        for state, output in list[Tuple[_State, _StateAndResult]]([
+            ([1, 2], ([], 3)),
+            ([1, 2, 3], ([3], 3)),
+        ]):
+            with self.subTest(state=state, output=output):
+                self.assertEqual(
+                    and_(eq(1), eq(2))(_Scope(), state),
+                    output
+                )
+
+    def test_apply_fail(self):
+        for state in list[_State]([
+            [],
+            [1],
+            [2],
+            [1, 3],
+        ]):
+            with self.subTest(state=state):
+                with self.assertRaises(errors.Error):
+                    and_(eq(1), eq(2))(_Scope(), state)
+
+
+class ZeroOrMoreTest(unittest.TestCase):
+    def test_apply(self):
+        for state, output in list[Tuple[_State, _StateAndResult]]([
+            ([], ([], 0)),
+            ([1], ([], 1)),
+            ([1, 1], ([], 2)),
+            ([2], ([2], 0)),
+            ([1, 2], ([2], 1)),
+            ([1, 1, 2], ([2], 2)),
+        ]):
+            with self.subTest(state=state, output=output):
+                self.assertEqual(
+                    zero_or_more(eq(1))(_Scope(), state),
+                    output
+                )
+
+
+class OneOrMoreTest(unittest.TestCase):
+    def test_apply(self):
+        for state, output in list[Tuple[_State, _StateAndResult]]([
+            ([1], ([], 1)),
+            ([1, 1], ([], 2)),
+            ([1, 2], ([2], 1)),
+            ([1, 1, 2], ([2], 2)),
+        ]):
+            with self.subTest(state=state, output=output):
+                self.assertEqual(
+                    one_or_more(eq(1))(_Scope(), state),
+                    output
+                )
+
+    def test_apply_fail(self):
+        for state in list[_State]([
+            ([], ([], 0)),
+            ([2], ([2], 0)),
+        ]):
+            with self.subTest(state=state):
+                with self.assertRaises(errors.Error):
+                    one_or_more(eq(1))(_Scope(), state)
+
+
+class ZeroOrOneTest(unittest.TestCase):
+    def test_apply(self):
+        for state, output in list[Tuple[_State, _StateAndResult]]([
+            ([], ([], 0)),
+            ([1], ([], 1)),
+            ([2], ([2], 0)),
+            ([1, 2], ([2], 1)),
+        ]):
+            with self.subTest(state=state, output=output):
+                self.assertEqual(
+                    zero_or_one(eq(1))(_Scope(), state),
+                    output
+                )
