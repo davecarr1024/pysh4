b097d20622bb45844cdd8a3c4e4e0fe34735c867
Dave Carr <davecarr1024@gmail.com>
2022-10-21 12:39:09 -0400

    class tests

--- pype/pype_test.py
+++ pype/pype_test.py
@@ -48,6 +48,52 @@ class PypeTest(unittest.TestCase):
                 ''',
                 builtins_.int_(1),
             ),
+            (
+                r'''
+                class c {
+                    def __init__(self) {
+                        self.a = 2;
+                    }
+                }
+                o = c();
+                o.a = 1;
+                o.a;
+                ''',
+                builtins_.int_(1),
+            ),
+            (
+                r'''
+                class c {
+                    def __init__(self) {
+                        self.a = 2;
+                    }
+                    def get(self) {
+                        return self.a / 2;
+                    }
+                }
+                c().get();
+                ''',
+                builtins_.int_(1),
+            ),
+            (
+                r'''
+                class c {
+                    def __init__(self) {
+                        self.a = 0;
+                    }
+                    def set(self, a) {
+                        self.a = a;
+                    }
+                    def get(self) {
+                        return self.a;
+                    }
+                }
+                o = c();
+                o.set(1);
+                o.get();
+                ''',
+                builtins_.int_(1),
+            ),
         ]):
             with self.subTest(input=input, result=result):
                 self.assertEqual(pype.eval(input), result)
